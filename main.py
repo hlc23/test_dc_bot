bb66c57b26049813c82dbdb5042545bfd0e08d18
leeuho16 <henry.lee.code23@gmail.com>
2022-05-01 15:22:58 +0800

    make Bot class

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import discord
 import logging
 
+# 設定日誌
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('')
-    print(f"{client.user} has online.")
-    print("="*15)
+class Bot(discord.Client):
 
-@client.event
-async def on_message(message):
-    # 當訊息
-    if message.author == client.user:
-        return
 
-    if message.content.startswith(f'!Hello'):
-        print(f'{message.author} called "!Hello"')
-        await message.channel.send('Hello')
+    # 登入bot提示訊息
+    async def on_ready(self):
+        print('')
+        print(f"{bot.user} has online.")
+        print("="*15)
 
-client.run(input('enter your bot token\n'))
+    # 訊息判斷
+    async def on_message(self,message):
+        # 避免被自己的訊息觸發
+        if message.author == self.user:
+            return
+
+        
+        if message.content.startswith(f'!Hello'):
+            print(f'{message.author} called "!Hello"')
+            await message.reply('Hello')
+
+bot = Bot()
+bot.run(input('enter your bot token\n'))
