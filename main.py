f4acf0acdb16c232516949bc393de846c0184da4
leeuho16 <henry.lee.code23@gmail.com>
2022-05-01 14:52:33 +0800

    basic bot with log
    bot can say "Hello" when you enter "!Hello" in any channel which he can see

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 import discord
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('')
+    print(f"{client.user} has online.")
+    print("="*15)
+
+@client.event
+async def on_message(message):
+    # 當訊息
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(f'!Hello'):
+        print(f'{message.author} called "!Hello"')
+        await message.channel.send('Hello')
+
+client.run(input('enter your bot token\n'))
