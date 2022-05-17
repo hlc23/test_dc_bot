d7333d8e07fa656fcb611643d23b3c0cf441b786
henryleecode23 <99625462+henryleecode23@users.noreply.github.com>
2022-05-17 00:13:55 +0000

    feat:add search

--- lib/pixiv.py
+++ lib/pixiv.py
@@ -11,3 +11,20 @@ def recommended(path:str="./", n:int=0):
     json_result = api.illust_recommended()
     for t in range(n):
         api.download(json_result.illusts[t].image_urls.medium,path=path,fname=f"image{t+1}.png")
+
+
+def search(key:str, n:int):
+    api = AppPixivAPI()
+    api.auth(refresh_token=REFRESH_TOKEN)
+
+
+    json_result = api.search_illust(key)
+    n = n
+    for illust in json_result.illusts:
+        if n <= 0:
+            break
+        api.download(illust.image_urls.medium, path="./data/image/")
+        n -= 1
+
+
+
