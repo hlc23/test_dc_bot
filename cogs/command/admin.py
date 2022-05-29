a8c1d8be7963ed13e07a5a4c0d42bcd7d92d5a08
leeuho16 <henry.lee.code23@gmail.com>
2022-05-29 18:38:47 +0800

    fix: role check

--- cogs/command/admin.py
+++ cogs/command/admin.py
@@ -2,18 +2,21 @@ import discord
 from core.cog_class import Cog_basic
 from discord.ext import commands
 
+def is_admin(ctx):
+    for role in ctx.author.roles:
+        if 969962769854128240 == role.id:
+            return True
+    return False
+
+
 class Admin(Cog_basic):
 
     @commands.Command
+    @commands.check(is_admin)
     async def say(self, ctx:commands.Context, channel:discord.TextChannel, msg):
-        guild = self.bot.get_guild(self.config["HLCT_guild"])
-        guild :discord.Guild
-        admin = guild.get_role(self.config["admin_role"])
-        if admin not in ctx.author.roles:
-            return
         await ctx.message.delete()
         await channel.send(msg)
-    
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Admin(bot))
--- core/cog_class.py
+++ core/cog_class.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import json
-from typing import List
 
 class Cog_basic(commands.Cog):
     def __init__(self, bot:commands.Bot):
@@ -11,18 +10,3 @@ class Cog_basic(commands.Cog):
         self.guild = self.bot.get_guild(self.config["HLCT_guild"])
         self.guild: discord.Guild
 
-    def is_role(self, ctx:commands.Context, role_id:int):
-        '''
-        @commands.Command
-        @commands.check(is_role)
-        async def ping(self, ctx):
-            pass
-        '''
-        return self.guild.get_role(role_id) in ctx.author.roles
-    
-    def is_roles(self, ctx:commands.Context, role_ids:List[int]):
-        for role_id in role_ids:
-            role = self.guild.get_role(role_id)
-            if role in ctx.author.roles:
-                return True
-        return False
