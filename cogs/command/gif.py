8e3823e28e3b91e37e75400c903b59d0c5b0eb91
leeuho16 <henry.lee.code23@gmail.com>
2022-06-04 17:40:22 +0800

    fix : bad embed and result none respond

--- cogs/command/gif.py
+++ cogs/command/gif.py
@@ -7,11 +7,15 @@ class Gif(Cog_basic):
 
     @commands.Command
     async def tenor(self, ctx:commands.Context, keyword:str):
+        result = gifsearch(keyword)
+        if result is None:
+            await ctx.send("查無結果")
+            return
         embed = Embed(title=keyword)
-        embed.set_image(url=gifsearch(keyword))
-        embed.set_author(name=ctx.author)
+        embed.set_image(url=result)
         embed.set_footer(icon_url=ctx.author.avatar_url, text=ctx.author.display_name)
         await ctx.send(embed=embed)
+        await ctx.message.delete()
 
 
 def setup(bot:commands.Bot):
