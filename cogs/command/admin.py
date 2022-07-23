65844a40fd6b0db4cb2d3f87495cc4f1488f0eaf
henryleecode23 <99625462+henryleecode23@users.noreply.github.com>
2022-07-24 00:54:51 +0800

    feat: change status like "online" "offline"

--- cogs/command/admin.py
+++ cogs/command/admin.py
@@ -4,10 +4,8 @@ from discord.ext import commands
 
 class Admin(Cog_basic):
 
-
     @discord.slash_command(name="join", description="ONLY ADMIN:Let someone in.")
     async def join(self, ctx:discord.ApplicationContext, member:discord.Member):
-        print(ctx.author.guild_permissions.administrator)
         if ctx.author.guild_permissions.administrator is False:
             embed = discord.Embed(title="您無使用此指令的權限", colour=discord.Colour.red())
             await ctx.respond(embed=embed, ephemeral=True)
@@ -23,8 +21,37 @@ class Admin(Cog_basic):
         await ctx.respond(f"{member.mention} 已成功加入", ephemeral=True)
 
     @discord.slash_command(name="status", description="ONLY ADMIN:bot status.")
-    async def status():
-        pass
+    async def status(
+        self,
+        ctx:discord.ApplicationContext,
+        status:discord.Option(description="bot status", choices=["online", "offline", "idle", "dnd", "invisible"])
+        ):
+        if ctx.author.guild_permissions.administrator is False:
+            embed = discord.Embed(title="您無使用此指令的權限", colour=discord.Colour.red())
+            await ctx.respond(embed=embed, ephemeral=True)
+            return
+        if status == "online":
+            await self.bot.change_presence(status=discord.Status.online)
+            await ctx.respond("Change success", ephemeral=True)
+            return
+        elif status == "offline":
+            await self.bot.change_presence(status=discord.Status.offline)
+            await ctx.respond("Change success", ephemeral=True)
+            return
+        elif status == "idle":
+            await self.bot.change_presence(status=discord.Status.idle)
+            await ctx.respond("Change success", ephemeral=True)
+            return
+        elif status == "dnd":
+            await self.bot.change_presence(status=discord.Status.dnd)
+            await ctx.respond("Change success", ephemeral=True)
+            return
+        elif status == "invisible":
+            await self.bot.change_presence(status=discord.Status.invisible)
+            await ctx.respond("Change success", ephemeral=True)
+            return
+
+        
 
 def setup(bot: discord.Bot):
     bot.add_cog(Admin(bot))
