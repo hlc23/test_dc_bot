e8032e67237527263a030ee2c879beed474d9e10
henryleecode23 <99625462+henryleecode23@users.noreply.github.com>
2023-04-15 16:26:28 +0800

    feat: special fxxk sentence

--- cogs/event/respond.py
+++ cogs/event/respond.py
@@ -2,10 +2,8 @@ from core.cog_class import Cog_basic
 
 import discord
 from discord.ext import commands
-from discord import File
 from random import choice
 from yaml import safe_load
-from requests import get
 
 class Respond(Cog_basic):
 
@@ -15,9 +13,13 @@ class Respond(Cog_basic):
             return
         if msg.content == "艸":
             with open("./data/fuck_text.yaml", "r", encoding="utf-8") as f:
-                await msg.reply(choice(safe_load(f)))
+                text = choice(safe_load(f))
+            if text == "今天不開心沒關係，反正明天也不會開心":
+                await msg.channel.send("https://cdn.discordapp.com/attachments/967704354977103952/1096647529766080663/FB_IMG_1681531522298.jpg")
+            else:
+                await msg.channel.send(text)
             return
-        
+
         if msg.author.guild_permissions.administrator:
             if msg.content.startswith(".") and msg.content[1:4] == "say":     
                 await msg.delete()
