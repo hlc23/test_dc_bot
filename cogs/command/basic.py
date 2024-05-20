8d08c9425d4bb821ac45cbf9df8c8d31fa8fae56
henryleecode23 <99625462+hlc23@users.noreply.github.com>
2024-05-20 09:59:57 +0800

    feat: use timestamp

--- cogs/command/basic.py
+++ cogs/command/basic.py
@@ -1,5 +1,7 @@
 from core.cog_class import Cog_basic
+from discord.utils import format_dt
 import discord
+import datetime
 
 class Basic(Cog_basic):
     
@@ -11,8 +13,12 @@ class Basic(Cog_basic):
     @discord.slash_command(name="me", description="查看伺服器上的個人資料")
     async def me(self, ctx:discord.ApplicationContext):
         embed=discord.Embed(title=ctx.author.name, description=ctx.author.mention)
-        joinTime = f"{ctx.author.joined_at.date()} {((ctx.author.joined_at.time().hour)+8):0>2}:{ctx.author.joined_at.time().minute:0>2}:{ctx.author.joined_at.time().second:0>2}"
-        embed.add_field(name="加入時間", value=joinTime, inline=False)
+        joinDate = ctx.author.joined_at.date()
+        joinTime = ctx.author.joined_at.time()
+
+        jT = datetime.datetime(joinDate.year, joinDate.month, joinDate.day, joinTime.hour, joinTime.minute, joinTime.second)
+
+        embed.add_field(name="加入時間", value=f"{format_dt(jT, 'f')}\n{format_dt(jT, 'R')}", inline=False)
         embed.add_field(name="狀態", value=ctx.author.activity, inline=False)
         embed.add_field(name="最高身分組", value=ctx.author.top_role, inline=False)
         embed.set_thumbnail(url=ctx.author.display_avatar.url)
@@ -22,12 +28,16 @@ class Basic(Cog_basic):
     async def show(
         self, 
         ctx:discord.ApplicationContext,
-        member:discord.Option(discord.Member, "查看的對象")
+        member:discord.Option(discord.Member, "查看的對象") # type: ignore
         ):
         self.member:discord.Member = member
         embed=discord.Embed(title=self.member.name, description=self.member.mention)
-        joinTime = f"{self.member.joined_at.date()} {((self.member.joined_at.time().hour)+8):0>2}:{self.member.joined_at.time().minute:0>2}:{self.member.joined_at.time().second:0>2}"
-        embed.add_field(name="加入時間", value=joinTime, inline=False)
+        joinDate = ctx.author.joined_at.date()
+        joinTime = ctx.author.joined_at.time()
+
+        jT = datetime.datetime(joinDate.year, joinDate.month, joinDate.day, joinTime.hour, joinTime.minute, joinTime.second)
+
+        embed.add_field(name="加入時間", value=f"{format_dt(jT, 'f')}\n{format_dt(jT, 'R')}", inline=False)
         embed.add_field(name="狀態", value=self.member.activity, inline=False)
         embed.add_field(name="最高身分組", value=self.member.top_role, inline=False)
         embed.set_thumbnail(url=self.member.display_avatar.url)
